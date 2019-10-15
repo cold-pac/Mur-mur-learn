dcf8ad051400ec117080db81eaced0f513397426
Erol Cimen <ilikememesandwanttodie@gmail.com>
2019-10-15 20:45:00 +1100

    I think I ruined it

--- logic.js
+++ logic.js
@@ -4,7 +4,8 @@ let soundVerbose = {'normal heart sounds':"normal heart sounds (S1 and S2, heard
     'aortic stenosis':"aortic stenosis (ejection-systolic, high-pitched murmur, crescendo-decrescendo character. Radiates to the carotids)",
     'aortic regurgitation':"aortic regurgitation (early diastolic, soft murmur)",
     'S3': 'S3: Sounds like lub de dub (Kentucky cadence), heard roughly 0.1 seconds after the second heart sound. Caused by rapid ventricular filling, causing the chordae tendinae to pull to their full length and twang like a guitar string.  indicates heart failure because the ventricles and chordae are stiff and weak and reach their limit much quicker than normal.',
-    'S4': ' heard before S1, sounds like le lub dub (Tenesse cadence). Caused by turbulent flow from an atria contracting against a non-compliant (stiff or hypertrophic) ventricle'
+    'S4': ' heard before S1, sounds like le lub dub (Tenesse cadence). Caused by turbulent flow from an atria contracting against a non-compliant (stiff or hypertrophic) ventricle',
+    'split S2': 'splitting of S2.'
 };
 
 let correctCounter = 0, totalCounter = 0;
@@ -105,6 +106,9 @@ document.onkeypress = function (event) {
             break;
         case "7":
             document.getElementById("S4").click();
+            break;
+        case "8":
+            document.getElementById("split_S2").click();
             break;
         case "p":
             document.getElementById("play").click();
